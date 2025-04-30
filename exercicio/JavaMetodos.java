34c8adcc4f68659cc24d5cfe7f61d004edc3b766
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2025-04-29 21:15:42 -0300

    Add files via upload

---
+++ exercicio/JavaMetodos.java
@@ -0,0 +1,13 @@
+package exercicio;
+
+public class JavaMetodos {
+    static void myMethod(){
+        System.out.println("I just got executed! ");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
