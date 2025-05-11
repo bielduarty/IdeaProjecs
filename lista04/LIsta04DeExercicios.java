f7744a6306d2ab52a374d83493ca29574e9fe955
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2025-05-11 18:50:14 -0300

    Add files via upload

---
+++ lista04/LIsta04DeExercicios.java
@@ -0,0 +1,29 @@
+//1 Verificação de Número Par ou Ímpar:
+//Escreva um programa que solicite ao usuário um número inteiro e verifique se ele é par ou ímpar.
+
+//2 Verificação de Maioridade:
+//Escreva um programa que solicite ao usuário sua idade e verifique se ele é maior de idade (18 anos ou mais).
+
+//3 Verificação de Nota:
+//Escreva um programa que solicite ao usuário uma nota de 0 a 100 e exiba se ele foi aprovado (nota >= 60) ou reprovado.
+
+//4 Verificação de Sinal de Número:
+//Escreva um programa que solicite ao usuário um número e verifique se ele é positivo, negativo ou zero.
+
+//5 Verificação de Ano Bissexto:
+//Escreva um programa que solicite ao usuário um ano e verifique se ele é bissexto.
+
+//6 Verificação de Maior Número:
+//Escreva um programa que solicite ao usuário três números e verifique qual deles é o maior.
+
+//7 Verificação de Intervalo:
+//Escreva um programa que solicite ao usuário um número e verifique se ele está no intervalo de 10 a 20 (inclusive).
+
+//8 Verificação de Divisibilidade:
+//Escreva um programa que solicite ao usuário um número e verifique se ele é divisível por 3 e por 5.
+
+//9 Verificação de Letra Vogal ou Consoante:
+//Escreva um programa que solicite ao usuário uma letra e verifique se ela é uma vogal ou uma consoante.
+
+//10 Verificação de Triângulo Válido:
+//Escreva um programa que solicite ao usuário os comprimentos dos três lados de um triângulo e verifique se eles formam um triângulo válido.
