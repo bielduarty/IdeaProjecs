92877cbcbf7a973a0e110ad80a5bb780884e89c5
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2025-05-27 21:30:32 -0300

    Add files via upload

---
+++ lista05/Lista05DeExercicios.java
@@ -0,0 +1,29 @@
+//1 Verificação de Número Primo:
+//Escreva um programa que solicite ao usuário um número inteiro e verifique se ele é um número primo.
+
+//2 Classificação de Triângulos:
+//Escreva um programa que solicite ao usuário os comprimentos dos três lados de um triângulo e classifique-o como equilátero, isósceles ou escaleno.
+
+// 3Cálculo de IMC:
+//Escreva um programa que solicite ao usuário seu peso e altura, calcule o Índice de Massa Corporal (IMC) e classifique-o de acordo com a tabela de IMC.
+
+// 4Verificação de Ano Bissexto:
+//Escreva um programa que solicite ao usuário um ano e verifique se ele é bissexto.
+
+// 5Cálculo de Média e Classificação:
+//Escreva um programa que solicite ao usuário três notas, calcule a média e exiba a classificação (Aprovado, Recuperação, Reprovado).
+
+// 6Verificação de Paridade e Positividade:
+//Escreva um programa que solicite ao usuário um número inteiro e verifique se ele é par ou ímpar, e se é positivo, negativo ou zero.
+
+// 7Verificação de Nota e Conceito:
+//Escreva um programa que solicite ao usuário uma nota de 0 a 100 e exiba o conceito correspondente (A, B, C, D, F).
+
+// 8Verificação de Categoria de Idade:
+//Escreva um programa que solicite ao usuário sua idade e exiba a categoria correspondente (Criança, Adolescente, Adulto, Idoso).
+
+// 9Verificação de Sinal de Número:
+//Escreva um programa que solicite ao usuário um número e verifique se ele é positivo, negativo ou zero.
+
+// 10Verificação de Divisibilidade:
+//Escreva um programa que solicite ao usuário um número e verifique se ele é divisível por 3 e por 5.
