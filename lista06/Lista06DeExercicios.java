ba05803e2f4dbc84d01467e4ac44ff3597105728
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2025-05-27 22:41:57 -0300

    Add files via upload

---
+++ lista06/Lista06DeExercicios.java
@@ -0,0 +1,20 @@
+//1) Verificação de Dia da Semana:
+//Escreva um programa que solicite ao usuário um número de 1 a 7 e exiba o dia da semana correspondente (1 para Domingo, 2 para Segunda, etc.).
+//
+//2) Verificação de Mês do Ano:
+//Escreva um programa que solicite ao usuário um número de 1 a 12 e exiba o mês do ano correspondente.
+//
+//3) Calculadora Simples:
+//Escreva um programa que solicite ao usuário dois números e uma operação (adição, subtração, multiplicação, divisão) e exiba o resultado.
+//
+//4) Verificação de Estação do Ano:
+//Escreva um programa que solicite ao usuário um número de 1 a 4 e exiba a estação do ano correspondente (1 para Primavera, 2 para Verão, etc.).
+//
+//5) Verificação de Categoria de Idade:
+//Escreva um programa que solicite ao usuário sua idade e exiba a categoria correspondente (Criança, Adolescente, Adulto, Idoso).
+//
+//6)  Verificação de Tipo de Triângulo:
+//Escreva um programa que solicite ao usuário os comprimentos dos três lados de um triângulo e classifique-o como equilátero, isósceles ou escaleno.
+//
+//7) Verificação de Mês e Estação:
+//Escreva um programa que solicite ao usuário um número de 1 a 12 e exiba o mês e a estação do ano correspondente.
